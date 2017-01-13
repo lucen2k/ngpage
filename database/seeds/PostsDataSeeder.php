5935f1f5fc075e29347125b746c34b7fe65414ca
Lucen jeong <lucen2k@gmail.com>
2017-01-13 07:06:05 +0000

    #5 エラー修正

--- database/seeds/PostsDataSeeder.php
+++ database/seeds/PostsDataSeeder.php
@@ -20,7 +20,12 @@ class PostsDataSeeder extends Seeder
         ]);
         Post::create([
             'title' => 'Best PHP framework 2',
-            'content' => 'Best PHP framework Best PHP framework Best PHP framework Best PHP framework'
+            'content' => 'Best PHP framework Best PHP framework Best PHP framework Best PHP framework 
+            Best PHP framework Best PHP framework Best PHP framework Best PHP framework 
+            Best PHP framework Best PHP framework Best PHP framework Best PHP framework 
+            Best PHP framework Best PHP framework Best PHP framework Best PHP framework 
+            Best PHP framework Best PHP framework Best PHP framework Best PHP framework 
+            Best PHP framework Best PHP framework Best PHP framework Best PHP framework'
         ]);
         Post::create([
             'title' => 'Best PHP framework 3',
@@ -36,6 +41,267 @@ class PostsDataSeeder extends Seeder
         ]);
         Post::create([
             'title' => 'Best PHP framework 6',
+            'content' => 'Best PHP framework Best PHP framework Best PHP framework Best PHP framework'
+        ]);
+        Post::create([
+            'title' => 'Best PHP framework 7',
+            'content' => 'Best PHP framework Best PHP framework Best PHP framework Best PHP framework'
+        ]);
+        Post::create([
+            'title' => 'Best PHP framework 8',
+            'content' => 'Best PHP framework Best PHP framework Best PHP framework Best PHP framework 
+            Best PHP framework Best PHP framework Best PHP framework Best PHP framework 
+            Best PHP framework Best PHP framework Best PHP framework Best PHP framework 
+            Best PHP framework Best PHP framework Best PHP framework Best PHP framework 
+            Best PHP framework Best PHP framework Best PHP framework Best PHP framework 
+            Best PHP framework Best PHP framework Best PHP framework Best PHP framework'
+        ]);
+        Post::create([
+            'title' => 'Best PHP framework 9',
+            'content' => 'Best PHP framework Best PHP framework Best PHP framework Best PHP framework'
+        ]);
+        Post::create([
+            'title' => 'Best PHP framework 10',
+            'content' => 'Best PHP framework Best PHP framework Best PHP framework Best PHP framework'
+        ]);
+        Post::create([
+            'title' => 'Best PHP framework 11',
+            'content' => 'Best PHP framework Best PHP framework Best PHP framework Best PHP framework'
+        ]);
+        Post::create([
+            'title' => 'Best PHP framework 12',
+            'content' => 'Best PHP framework Best PHP framework Best PHP framework Best PHP framework'
+        ]);
+        Post::create([
+            'title' => 'Best PHP framework 13',
+            'content' => 'Best PHP framework Best PHP framework Best PHP framework Best PHP framework'
+        ]);
+        Post::create([
+            'title' => 'Best PHP framework 14',
+            'content' => 'Best PHP framework Best PHP framework Best PHP framework Best PHP framework 
+            Best PHP framework Best PHP framework Best PHP framework Best PHP framework 
+            Best PHP framework Best PHP framework Best PHP framework Best PHP framework 
+            Best PHP framework Best PHP framework Best PHP framework Best PHP framework 
+            Best PHP framework Best PHP framework Best PHP framework Best PHP framework 
+            Best PHP framework Best PHP framework Best PHP framework Best PHP framework'
+        ]);
+        Post::create([
+            'title' => 'Best PHP framework 15',
+            'content' => 'Best PHP framework Best PHP framework Best PHP framework Best PHP framework'
+        ]);
+        Post::create([
+            'title' => 'Best PHP framework 16',
+            'content' => 'Best PHP framework Best PHP framework Best PHP framework Best PHP framework'
+        ]);
+        Post::create([
+            'title' => 'Best PHP framework 17',
+            'content' => 'Best PHP framework Best PHP framework Best PHP framework Best PHP framework'
+        ]);
+        Post::create([
+            'title' => 'Best PHP framework 18',
+            'content' => 'Best PHP framework Best PHP framework Best PHP framework Best PHP framework'
+        ]);
+        Post::create([
+            'title' => 'Best PHP framework 19',
+            'content' => 'Best PHP framework Best PHP framework Best PHP framework Best PHP framework'
+        ]);
+        Post::create([
+            'title' => 'Best PHP framework 20',
+            'content' => 'Best PHP framework Best PHP framework Best PHP framework Best PHP framework 
+            Best PHP framework Best PHP framework Best PHP framework Best PHP framework 
+            Best PHP framework Best PHP framework Best PHP framework Best PHP framework 
+            Best PHP framework Best PHP framework Best PHP framework Best PHP framework 
+            Best PHP framework Best PHP framework Best PHP framework Best PHP framework 
+            Best PHP framework Best PHP framework Best PHP framework Best PHP framework'
+        ]);
+        Post::create([
+            'title' => 'Best PHP framework 21',
+            'content' => 'Best PHP framework Best PHP framework Best PHP framework Best PHP framework'
+        ]);
+        Post::create([
+            'title' => 'Best PHP framework 22',
+            'content' => 'Best PHP framework Best PHP framework Best PHP framework Best PHP framework'
+        ]);
+        Post::create([
+            'title' => 'Best PHP framework 23',
+            'content' => 'Best PHP framework Best PHP framework Best PHP framework Best PHP framework'
+        ]);
+        Post::create([
+            'title' => 'Best PHP framework 24',
+            'content' => 'Best PHP framework Best PHP framework Best PHP framework Best PHP framework'
+        ]);
+        Post::create([
+            'title' => 'Best PHP framework 25',
+            'content' => 'Best PHP framework Best PHP framework Best PHP framework Best PHP framework'
+        ]);
+        Post::create([
+            'title' => 'Best PHP framework 26',
+            'content' => 'Best PHP framework Best PHP framework Best PHP framework Best PHP framework 
+            Best PHP framework Best PHP framework Best PHP framework Best PHP framework 
+            Best PHP framework Best PHP framework Best PHP framework Best PHP framework 
+            Best PHP framework Best PHP framework Best PHP framework Best PHP framework 
+            Best PHP framework Best PHP framework Best PHP framework Best PHP framework 
+            Best PHP framework Best PHP framework Best PHP framework Best PHP framework'
+        ]);
+        Post::create([
+            'title' => 'Best PHP framework 27',
+            'content' => 'Best PHP framework Best PHP framework Best PHP framework Best PHP framework'
+        ]);
+        Post::create([
+            'title' => 'Best PHP framework 28',
+            'content' => 'Best PHP framework Best PHP framework Best PHP framework Best PHP framework'
+        ]);
+        Post::create([
+            'title' => 'Best PHP framework 29',
+            'content' => 'Best PHP framework Best PHP framework Best PHP framework Best PHP framework'
+        ]);
+        Post::create([
+            'title' => 'Best PHP framework 30',
+            'content' => 'Best PHP framework Best PHP framework Best PHP framework Best PHP framework'
+        ]);
+        Post::create([
+            'title' => 'Best PHP framework 31',
+            'content' => 'Best PHP framework Best PHP framework Best PHP framework Best PHP framework'
+        ]);
+        Post::create([
+            'title' => 'Best PHP framework 32',
+            'content' => 'Best PHP framework Best PHP framework Best PHP framework Best PHP framework 
+            Best PHP framework Best PHP framework Best PHP framework Best PHP framework 
+            Best PHP framework Best PHP framework Best PHP framework Best PHP framework 
+            Best PHP framework Best PHP framework Best PHP framework Best PHP framework 
+            Best PHP framework Best PHP framework Best PHP framework Best PHP framework 
+            Best PHP framework Best PHP framework Best PHP framework Best PHP framework'
+        ]);
+        Post::create([
+            'title' => 'Best PHP framework 33',
+            'content' => 'Best PHP framework Best PHP framework Best PHP framework Best PHP framework'
+        ]);
+        Post::create([
+            'title' => 'Best PHP framework 34',
+            'content' => 'Best PHP framework Best PHP framework Best PHP framework Best PHP framework'
+        ]);
+        Post::create([
+            'title' => 'Best PHP framework 35',
+            'content' => 'Best PHP framework Best PHP framework Best PHP framework Best PHP framework'
+        ]);
+        Post::create([
+            'title' => 'Best PHP framework 36',
+            'content' => 'Best PHP framework Best PHP framework Best PHP framework Best PHP framework'
+        ]);
+        Post::create([
+            'title' => 'Best PHP framework 37',
+            'content' => 'Best PHP framework Best PHP framework Best PHP framework Best PHP framework'
+        ]);
+        Post::create([
+            'title' => 'Best PHP framework 38',
+            'content' => 'Best PHP framework Best PHP framework Best PHP framework Best PHP framework 
+            Best PHP framework Best PHP framework Best PHP framework Best PHP framework 
+            Best PHP framework Best PHP framework Best PHP framework Best PHP framework 
+            Best PHP framework Best PHP framework Best PHP framework Best PHP framework 
+            Best PHP framework Best PHP framework Best PHP framework Best PHP framework 
+            Best PHP framework Best PHP framework Best PHP framework Best PHP framework'
+        ]);
+        Post::create([
+            'title' => 'Best PHP framework 39',
+            'content' => 'Best PHP framework Best PHP framework Best PHP framework Best PHP framework'
+        ]);
+        Post::create([
+            'title' => 'Best PHP framework 40',
+            'content' => 'Best PHP framework Best PHP framework Best PHP framework Best PHP framework'
+        ]);
+        Post::create([
+            'title' => 'Best PHP framework 41',
+            'content' => 'Best PHP framework Best PHP framework Best PHP framework Best PHP framework'
+        ]);
+        Post::create([
+            'title' => 'Best PHP framework 42',
+            'content' => 'Best PHP framework Best PHP framework Best PHP framework Best PHP framework'
+        ]);
+        Post::create([
+            'title' => 'Best PHP framework 43',
+            'content' => 'Best PHP framework Best PHP framework Best PHP framework Best PHP framework'
+        ]);
+        Post::create([
+            'title' => 'Best PHP framework 44',
+            'content' => 'Best PHP framework Best PHP framework Best PHP framework Best PHP framework 
+            Best PHP framework Best PHP framework Best PHP framework Best PHP framework 
+            Best PHP framework Best PHP framework Best PHP framework Best PHP framework 
+            Best PHP framework Best PHP framework Best PHP framework Best PHP framework 
+            Best PHP framework Best PHP framework Best PHP framework Best PHP framework 
+            Best PHP framework Best PHP framework Best PHP framework Best PHP framework'
+        ]);
+        Post::create([
+            'title' => 'Best PHP framework 45',
+            'content' => 'Best PHP framework Best PHP framework Best PHP framework Best PHP framework'
+        ]);
+        Post::create([
+            'title' => 'Best PHP framework 46',
+            'content' => 'Best PHP framework Best PHP framework Best PHP framework Best PHP framework'
+        ]);
+        Post::create([
+            'title' => 'Best PHP framework 47',
+            'content' => 'Best PHP framework Best PHP framework Best PHP framework Best PHP framework'
+        ]);
+        Post::create([
+            'title' => 'Best PHP framework 48',
+            'content' => 'Best PHP framework Best PHP framework Best PHP framework Best PHP framework'
+        ]);
+        Post::create([
+            'title' => 'Best PHP framework 49',
+            'content' => 'Best PHP framework Best PHP framework Best PHP framework Best PHP framework'
+        ]);
+        Post::create([
+            'title' => 'Best PHP framework 50',
+            'content' => 'Best PHP framework Best PHP framework Best PHP framework Best PHP framework 
+            Best PHP framework Best PHP framework Best PHP framework Best PHP framework 
+            Best PHP framework Best PHP framework Best PHP framework Best PHP framework 
+            Best PHP framework Best PHP framework Best PHP framework Best PHP framework 
+            Best PHP framework Best PHP framework Best PHP framework Best PHP framework 
+            Best PHP framework Best PHP framework Best PHP framework Best PHP framework'
+        ]);
+        Post::create([
+            'title' => 'Best PHP framework 51',
+            'content' => 'Best PHP framework Best PHP framework Best PHP framework Best PHP framework'
+        ]);
+        Post::create([
+            'title' => 'Best PHP framework 52',
+            'content' => 'Best PHP framework Best PHP framework Best PHP framework Best PHP framework'
+        ]);
+        Post::create([
+            'title' => 'Best PHP framework 53',
+            'content' => 'Best PHP framework Best PHP framework Best PHP framework Best PHP framework'
+        ]);
+        Post::create([
+            'title' => 'Best PHP framework 54',
+            'content' => 'Best PHP framework Best PHP framework Best PHP framework Best PHP framework'
+        ]);
+        Post::create([
+            'title' => 'Best PHP framework 55',
+            'content' => 'Best PHP framework Best PHP framework Best PHP framework Best PHP framework'
+        ]);
+        Post::create([
+            'title' => 'Best PHP framework 56',
+            'content' => 'Best PHP framework Best PHP framework Best PHP framework Best PHP framework 
+            Best PHP framework Best PHP framework Best PHP framework Best PHP framework 
+            Best PHP framework Best PHP framework Best PHP framework Best PHP framework 
+            Best PHP framework Best PHP framework Best PHP framework Best PHP framework 
+            Best PHP framework Best PHP framework Best PHP framework Best PHP framework 
+            Best PHP framework Best PHP framework Best PHP framework Best PHP framework'
+        ]);
+        Post::create([
+            'title' => 'Best PHP framework 57',
+            'content' => 'Best PHP framework Best PHP framework Best PHP framework Best PHP framework'
+        ]);
+        Post::create([
+            'title' => 'Best PHP framework 58',
+            'content' => 'Best PHP framework Best PHP framework Best PHP framework Best PHP framework'
+        ]);
+        Post::create([
+            'title' => 'Best PHP framework 59',
+            'content' => 'Best PHP framework Best PHP framework Best PHP framework Best PHP framework'
+        ]);
+        Post::create([
+            'title' => 'Best PHP framework 60',
             'content' => 'Best PHP framework Best PHP framework Best PHP framework Best PHP framework'
         ]);
     }
